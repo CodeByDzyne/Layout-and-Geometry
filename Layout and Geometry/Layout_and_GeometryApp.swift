257023c32eb3a5d83689e5e5d4f11c39b5d72686
CodeByDzyne <wdenley@dzynesolutions.com>
2023-05-04 21:50:35 +1000

    Initial Commit

---
+++ Layout and Geometry/Layout_and_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_and_GeometryApp.swift
+//  Layout and Geometry
+//
+//  Created by Wealthy Waz on 4/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Layout_and_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
